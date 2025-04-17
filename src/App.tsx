6112d168a3ead26365521670f6691a81f910d410
Victory Oseiwe <thisisvictoryoseiwe@gmail.com>
2025-04-17 16:12:51 +0100

    adding and creating props and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Home from "./pages/Home";
+
 export default function App() {
   return (
     <>
-      <div className=" bg-skyBlue rounded-b-md ">
-        <h1 className="font-bold text-2xl animate-fade-in-scale">
-          This is just a heading text
-        </h1>
+      <div className="flex flex-col bg-skyBlue rounded-b-md ">
+        <Home />
+        <Home />
+        <Home />
+        <Home />
       </div>
     </>
   );
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,26 @@
+import { Button } from "../common/Button";
+
+export default function Header() {
+  const handleAuth = () => {
+    console.log("Auth button clicked");
+  };
+  return (
+    <>
+      <div className="flex  flex-row justify-between align-middle rounded-md bg-lightMist px-7 py-5 w-full">
+        <h1 className="font-black ">FF</h1>
+        <ul className="flex align-middle gap-3 justify-between">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Ts</li>
+        </ul>
+        <Button
+          className="bg-chiliSpice text-creamyWhite px-3 py-2 rounded-md "
+          onClick={handleAuth}
+        >
+          Log in
+        </Button>
+      </div>
+    </>
+  );
+}
