80ce8a18d16c98886d7ba57e56f36ce4e4201658
Victory Oseiwe <thisisvictoryoseiwe@gmail.com>
2025-06-07 15:52:14 +0100

    Set maps to a specified location:

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,27 @@ import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import Hero from "../components/ui/Hero";
 import DeliverySectionInfo from "../components/ui/DeliverySectionInfo";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [userLocation, setUserLocation] = useState<string>("");
+
+  useEffect(() => {
+    const savedLocation = localStorage.getItem("location");
+    if (savedLocation) {
+      setUserLocation(savedLocation);
+    }
+  }, []);
   return (
     <>
       <div className="bg-lightMist text-charcoalSlate dark:bg-charcoalSlate/50 dark:text-creamyWhite">
-        <Header />
+        <div className="bg-amber-400">
+          <Header />
+        </div>
         <Hero />
+        <p className="font-bold text-2xl bg-ctaColor p-2 rounded-2xl">
+          {userLocation}
+        </p>
         <DeliverySectionInfo />
         <Footer />
       </div>
